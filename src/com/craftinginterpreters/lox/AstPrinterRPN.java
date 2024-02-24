9d88bbdad2a5bcab9f9a21d1acd6aa7ab1454b04
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-02-24 14:27:29 -0500

    feat: add inverse polish notation as a challenge

---
+++ src/com/craftinginterpreters/lox/AstPrinterRPN.java
@@ -0,0 +1,58 @@
+package com.craftinginterpreters.lox;
+
+public class AstPrinterRPN implements Expr.Visitor<String> {
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Binary(
+                        new Expr.Grouping(new Expr.Literal(1)),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Grouping(
+                                new Expr.Literal(2))
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                        new Expr.Grouping(new Expr.Literal(4)),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Grouping(
+                                new Expr.Literal(3)
+                        )
+                )
+        );
+
+
+        System.out.println(new AstPrinterRPN().print(expression));
+    }
+
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) {
+            return "nil";
+        }
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        String operator = expr.operator.lexeme;
+        if (expr.operator.type == TokenType.MINUS) {
+            operator = "neg ";
+        }
+        return expr.right.accept(this) + " " + operator;
+    }
+}
